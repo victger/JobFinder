d49297e241ffc6214646d56fd23874fcbc155cb8
lakhdaya <yassine.lakhdari2@edu.esiee.fr>
2023-10-28 00:45:24 +0200

    add docker files

---
+++ app/main.py
@@ -0,0 +1,59 @@
+from datetime import datetime, date
+from pydantic import BaseModel, Field
+from uuid import uuid4
+from typing_extensions import Annotated
+from typing import Optional
+
+from minio import Minio
+from fastapi import FastAPI, File, UploadFile
+
+from datetime import timedelta
+
+app = FastAPI(
+    title="My title",
+    description="My description",
+    version="0.0.1",
+)
+
+client = Minio("minio:9000", "root", "rootpassword", secure=False)
+
+
+
+@app.post('/file/upload/')
+async def upload_file(file: UploadFile = File(...)):
+    result = client.put_object("name", file.filename, file.file, 5000)
+    return {'result': result}
+
+
+
+@app.put('/file/presigned/')
+async def put_presigned_file(name: str):
+    put_url  = client.get_presigned_url(
+        "GET",
+        "name",
+        name,
+        expires=timedelta(days=1),
+        response_headers={"response-content-type": "application/json"},
+    )
+    return put_url
+
+@app.get('/file/presigned/')
+async def get_presigned_file(name: str):
+    put_url  = client.get_presigned_url(
+        "GET",
+        "name",
+        name,
+        expires=timedelta(days=1),
+        response_headers={"response-content-type": "application/json"},
+    )
+    return put_url
+
+@app.get('/bucket/show/')
+async def show_bucket(name: str):
+    objects = client.list_objects(name, recursive=True)
+    return {'result': '\n'.join([object.object_name for object in objects])}
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
--- front.py
+++ front.py
@@ -1,9 +1,9 @@
-import streamlit as st
+# import streamlit as st
 
-# I want a white background and in the middle of the page i want a form when you can enter your age
+# # I want a white background and in the middle of the page i want a form when you can enter your age
 
 
-age = st.number_input('Enter your age', 0, 100, 25)
-st.write('Your age is: ', age)
-salary = st.slider('Enter your salary in k$', 0, 100, 50)
-st.write('Your salary is: ', salary, 'k$')
+# age = st.number_input('Enter your age', 0, 100, 25)
+# st.write('Your age is: ', age)
+# salary = st.slider('Enter your salary in k$', 0, 100, 50)
+# st.write('Your salary is: ', salary, 'k$')
