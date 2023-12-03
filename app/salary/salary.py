91e41b7d50bd3fb760a46af7bf0946b1f174da92
lakhdaya <yassine.lakhdari2@edu.esiee.fr>
2023-12-03 20:03:56 +0100

    fixing page bugs and add cv

--- app/salary/salary.py
+++ app/salary/salary.py
@@ -1,13 +1,32 @@
 
-from fastapi import Depends, APIRouter, Request
+from fastapi import Depends, APIRouter, Request, File, UploadFile
+
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from db.services.db import get_db
 
-
+from user.services.user import get_current_user
 
 from salary.services.salary import *
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Fonction de validation du token
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # Vous pouvez ici vérifier la validité du token
+    print("token type", type(token))
+    print("_"*20)
+    if type(token) is not str:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return token
 
 salariesRouter = APIRouter()
 
@@ -19,25 +38,23 @@ templates = Jinja2Templates(directory="salary/templates")
 
 @salariesRouter.get("/salary", response_class=HTMLResponse)
 async def show_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "page": 1, "total_pages": 1})
+    print(request.cookies)
+    return templates.TemplateResponse("index.html", {"request": request, "page": 1, "total_pages": 1, "loc": "US", "desc": "Data"})
 
-@salariesRouter.get("/salary/show_from_loc/")
-async def show_from_loc(loc: str, db = Depends(get_db)):
-    return get_salaries_from_loc(loc, db=db)
 
-@salariesRouter.get("/salary/show_stats_from_loc/")
-async def show_stats_from_loc(request: Request, loc: str, db = Depends(get_db)):
-    stats =  get_stats_from_loc(loc, db=db)
-    salaries = get_salaries_from_loc(loc, db=db)
-    return templates.TemplateResponse("stats.html", {"request": request, "stats": stats, "location": loc, "salaries": salaries})
-
-@salariesRouter.get("/salary/show_from_desc/")
-async def show_from_desc(request: Request, desc: str, page: int=1, db = Depends(get_db)):
+@salariesRouter.get("/salary/show_salaries/")
+async def show_from_desc(request: Request, loc: str, desc: str, page: int=1, db = Depends(get_db)):
     items_per_page = 20
     offset = (page - 1) * items_per_page
-    res, total_pages = search_with_desc(desc, skip=offset, limit=items_per_page, db=db)
-    return templates.TemplateResponse("index.html", {"request": request, "salaries": res, "page": page, "total_pages": total_pages})
+    res, stats, total_pages = search_salaries(loc, desc, skip=offset, limit=items_per_page, db=db)
+    return templates.TemplateResponse("index.html", {"request": request, "salaries": res, "page": page, "total_pages": total_pages, "loc":loc, "desc": desc})
 
 @salariesRouter.get("/salary/show_cv/")
-async def show_cv():
-    return show_Mcv()
+async def show_cv(request: Request):
+    return show_Mcv(request.cookies["token"])
+
+@salariesRouter.post("/salary/add_cv/")
+async def add_cv(request: Request, file: UploadFile = File(...)):
+    add_Mcv(request.cookies["token"], file)
+    return {"message": "CV ajouté avec succès"}
+
--- app/salary/services/salary.py
+++ app/salary/services/salary.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-
+import io
 from sqlalchemy import func
 
-from fastapi import Depends
+from fastapi import Depends, UploadFile, Query
 from salary.models.salary import Salary
 
 from db.services.db import engine, get_db
@@ -11,6 +11,9 @@ from Minio.minio import client
 
 from io import BytesIO
 import PyPDF2
+from tkinter import Tk, filedialog
+
+from Minio.minio import client
 SALARY_PATH = "data/salaries.csv"
 
 
@@ -19,13 +22,6 @@ def create_salaries_from_csv(csv_path: str=SALARY_PATH):
     df = pd.read_csv(csv_path)
     df["id"] = np.arange(len(df))
     df.to_sql("salaries", engine, if_exists="replace", index=False)
-
-def get_salaries(skip=1, limit=10, db = Depends(get_db)):
-    res = db.query(Salary).offset(skip).limit(limit).all()
-    return res
-
-def get_salaries_from_loc(loc: str, skip=1, limit=10, db=Depends(get_db)):
-    return db.query(Salary).filter(Salary.company_location.ilike(loc)).offset(skip).limit(limit).all()
 
 def get_mean_salary(query):
     return query.session.query(func.avg(Salary.salary)).scalar()
@@ -39,19 +35,31 @@ def get_stats_from_loc(loc: str, db: Depends(get_db)=Depends(get_db)):
 def get_mean_salary(df):
     pass
 
-def show_linkedIn():
-    pass
+def search_with_loc_and_desc(query: Query, loc: str = None, desc: str = None):
+    if loc:
+        print(loc)
+        query = query.filter(func.lower(Salary.company_location).like(func.lower(f"%{loc}%")))
+    if desc:
+        query = query.filter(func.lower(Salary.job_title).like(func.lower(f"%{desc}%")))
+    return query
 
-def search_with_desc(desc: str, skip=1, limit=10, db=Depends(get_db)):
-    res = db.query(Salary).filter(Salary.job_title.ilike(desc))
-    total_pages = res.count() // limit + 1
-    res = res.offset(skip).limit(limit).all()
-    return res, total_pages
+def get_page(query: Query, skip=1, limit=10, ):
+    return query.offset(skip).limit(limit).all()
 
-def show_Mcv():
+def search_salaries(loc: str, desc: str, skip=1, limit=10, db=Depends(get_db)):
+    query = db.query(Salary)
+    query = search_with_loc_and_desc(query, loc, desc)
+    tot = query.count()
+    query = query.offset(skip).limit(limit)
+    page = query.all()
+    total_pages = tot // limit + 1
+    stats = None
+    return page, stats, total_pages
+
+def show_Mcv(user_token: str):
     pdf_data  = client.get_object(
         "users",
-        "test/Stage 6 mois AIRBUS.pdf",
+        f"{user_token}/cv.pdf",
     ).read()
     pdf_document = PyPDF2.PdfReader(BytesIO(pdf_data))
     text = ""
@@ -60,3 +68,8 @@ def show_Mcv():
             text += page.extract_text()
     return {"text": text}
 
+def add_Mcv(user_token: str, file: UploadFile):
+    content = file.file.read()
+    content_stream = io.BytesIO(content)
+    client.put_object("users", f"{user_token}/cv.pdf", content_stream, len(content))
+    return None
